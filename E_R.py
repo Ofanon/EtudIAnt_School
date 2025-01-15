47834df01340861244c4e4d93d878ae19f5959de
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-15 18:58:28 +0100

    Update E_R.py

--- E_R.py
+++ E_R.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.title("EtudIAnt : fiche de révision")
 
-genai.configure(api_key=st.text_input("Ta clée API"))
+genai.configure(api_key=st.chat_input("Ta clée API"))
 model = genai.GenerativeModel("gemini-1.5-flash-002")
 
 st.subheader("Sur quoi veux-tu créer une fiche de révision ?")
